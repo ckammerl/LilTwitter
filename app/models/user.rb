665af89d5697b0e694446ce0add356e1fbcdcc60
amdziuk <amdziuk@gmail.com>
2014-06-05 20:08:33 -0700

    Complete?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 class User < ActiveRecord::Base
   # users.password_hash in the database is a :string
-  has_many :tweets
+  include BCrypt
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  # include BCrypt
+  has_many :tweets
+
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
   # def password
   #   @password ||= Password.new(password_hash)
